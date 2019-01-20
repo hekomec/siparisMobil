4ce21eef137b1c4a8a2269b1161a65dd8de3284a
hekotech <haliskomec@gmail.com>
2019-01-20 19:50:44 +0300

    her bir hücre için kodlar bitmiş oldu

---
+++ app/src/main/java/com/udemy/hekotech/mobil_isletme/Views/LvIletisim.java
@@ -0,0 +1,69 @@
+package com.udemy.hekotech.mobil_isletme.Views;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.udemy.hekotech.mobil_isletme.Classes.Iletisim;
+import com.udemy.hekotech.mobil_isletme.Classes.Siparis;
+import com.udemy.hekotech.mobil_isletme.R;
+
+import java.util.List;
+
+/**
+ * Created by hekotech on 20.01.2019.
+ */
+
+public class LvIletisim extends RecyclerView.Adapter<LvIletisim.MyViewHolder> {
+
+
+private List<Iletisim> list;
+
+public class MyViewHolder extends RecyclerView.ViewHolder{
+
+    public TextView name,message,date,many;
+    public Button sil_iletisim;
+    public MyViewHolder(View itemView){
+        super(itemView);
+        name = itemView.findViewById(R.id.c_iletisim_name);
+        message = itemView.findViewById(R.id.c_iletisim_message);
+        date = itemView.findViewById(R.id.c_iletisim_date);
+        many = itemView.findViewById(R.id.c_iletisim_many);
+        sil_iletisim = itemView.findViewById(R.id.c_iletisim_sil);
+    }
+
+
+}
+
+    @NonNull
+    @Override
+    public LvIletisim.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.c_iletisim,parent,false);
+        return new LvIletisim.MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LvIletisim.MyViewHolder holder, int position) {
+        final Iletisim mylist = list.get(position);
+        holder.name.setText(mylist.getName());
+        holder.message.setText(mylist.getMessage());
+        holder.many.setText(mylist.getMany());
+        holder.date.setText(mylist.getDate());
+        holder.sil_iletisim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
---
+++ app/src/main/java/com/udemy/hekotech/mobil_isletme/Views/LvKurumsal.java
@@ -0,0 +1,69 @@
+package com.udemy.hekotech.mobil_isletme.Views;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.udemy.hekotech.mobil_isletme.Classes.Iletisim;
+import com.udemy.hekotech.mobil_isletme.Classes.Kurumsal;
+import com.udemy.hekotech.mobil_isletme.R;
+
+import java.util.List;
+
+/**
+ * Created by hekotech on 20.01.2019.
+ */
+
+public class LvKurumsal  extends RecyclerView.Adapter<LvKurumsal.MyViewHolder> {
+
+
+    private List<Kurumsal> list;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView name,message,date,exp;
+        public Button sil_kurumsal;
+        public MyViewHolder(View itemView){
+            super(itemView);
+            name = itemView.findViewById(R.id.c_kurumsal_name);
+            message = itemView.findViewById(R.id.c_kurumsal_message);
+            date = itemView.findViewById(R.id.c_kurumsal_date);
+            exp = itemView.findViewById(R.id.c_kurumsal_exp);
+            sil_kurumsal = itemView.findViewById(R.id.c_kurumsal_sil);
+        }
+
+
+    }
+
+    @NonNull
+    @Override
+    public LvKurumsal.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.c_kurumsal,parent,false);
+        return new LvKurumsal.MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LvKurumsal.MyViewHolder holder, int position) {
+        final Kurumsal mylist = list.get(position);
+        holder.name.setText(mylist.getName());
+        holder.message.setText(mylist.getMessage());
+        holder.exp.setText(mylist.getExp());
+        holder.date.setText(mylist.getDate());
+        holder.sil_kurumsal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
